a835684ebbc577e195b302b3d5d7fe162276316b
Askar Karasaev <askar.karasaev@gmail.com>
2020-11-06 14:29:06 +0900

    Preparing for heroku

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'askar'),
-        password: env('DATABASE_PASSWORD', 'karasish'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: "${process.env.DATABASE_HOST}",
+        port: "${process.env.DATABASE_PORT}",
+        database: "${process.env.DATABASE_NAME}",
+        username: "${process.env.DATABASE_USERNAME}",
+        password: "${process.env.DATABASE_PASSWORD}",
+        ssl: { rejectUnauthorized: false },
       },
-      options: {}
+      options: {},
     },
   },
 });
